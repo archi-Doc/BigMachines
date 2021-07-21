c249a1ede89a0da6a4d707a01d7aad862f69c44b
archi-Doc <archidoc422@gmail.com>
2021-07-21 17:51:30 +0900

    .

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,42 +9,42 @@ using Tinyhand;
 
 namespace ConsoleApp1
 {
-    [TinyhandObject(UseServiceProvider = true)] // Annotate TinyhandObject attribute and set UseServiceProvider to true if you want enable serialization.
+    [TinyhandObject(UseServiceProvider = true)] // Annotate TinyhandObject attribute to enable serialization (and set UseServiceProvider to true to skip default constructor check).
     [StateMachine(0)] // Annotate StateMachine and set Machine type id (unique number).
-    public partial class TestMachine : Machine<int> // Inherit Machine<TIdentifier>
+    public partial class TestMachine : Machine<int> // Inherit Machine<TIdentifier> class.
     {
         public TestMachine(BigMachine<int> bigMachine)
             : base(bigMachine)
         {
-            this.IsSerializable = true; // Serialize machine
-            this.DefaultTimeout = TimeSpan.FromSeconds(1); // Time interval for machine processing.
+            this.IsSerializable = true; // Set true to serialize the machine
+            this.DefaultTimeout = TimeSpan.FromSeconds(1); // Default time interval for machine processing.
             this.SetLifespan(TimeSpan.FromSeconds(10)); // Time until the machine automatically terminates.
         }
 
         [Key(10)]
         public int Count { get; set; }
 
-        [StateMethod(0)]
+        [StateMethod(0)] // Annotate StateMethod attribute and set state method id (0 for default state).
         protected StateResult Initial(StateParameter parameter)
         {// Inside lock (machine) statement.
             Console.WriteLine($"TestMachine {this.Identifier}: Initial");
 
-            this.ChangeState(TestMachine.State.One);
+            this.ChangeState(TestMachine.State.One); // Change to state One (The method name becomes the state name).
             return StateResult.Continue;
         }
 
         [StateMethod(1)]
         protected StateResult One(StateParameter parameter)
         {
-            Console.WriteLine($"TestMachine {this.Identifier}: One");
+            Console.WriteLine($"TestMachine {this.Identifier}: One - {this.Count}");
 
-            this.ChangeState(State.Two);
+            this.ChangeState(State.Two); // Try to change to state Two.
 
             return StateResult.Continue;
         }
 
         protected bool TwoCanEnter()
-        {
+        {// StateName + CanEnter() or CanExit() can control the transition of the state.
             var result = this.Count++ > 2;
             if (result)
             {
@@ -62,8 +62,13 @@ namespace ConsoleApp1
         [StateMethod(2)]
         protected StateResult Two(StateParameter parameter)
         {
-            Console.WriteLine($"TestMachine {this.Identifier}: Two");
+            Console.WriteLine($"TestMachine {this.Identifier}: Two - {this.Count++}");
             this.SetTimeout(TimeSpan.FromSeconds(0.5));
+
+            if (this.Count > 10)
+            {// Terminate the machine.
+                return StateResult.Terminate;
+            }
 
             return StateResult.Continue;
         }
@@ -72,8 +77,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine($"TestMachine {this.Identifier}: Terminated");
 
-            ThreadCore.Root.Terminate();
-            // this.BigMachine.Core.Terminate();
+            ThreadCore.Root.Terminate(); // Send a termination signal to the root.
         }
     }
 
@@ -93,13 +97,15 @@ namespace ConsoleApp1
                 ThreadCore.Root.Terminate(); // Send a termination signal to the root.
             };
 
-            var container = new Container(); // You can use DI container if you want.
+            /*var container = new Container(); // You can use DI container if you want.
             container.RegisterDelegate<BigMachine<int>>(x => new BigMachine<int>(ThreadCore.Root, container), Reuse.Singleton);
             container.Register<TestMachine>(Reuse.Transient);
-            var bigMachine = container.Resolve<BigMachine<int>>();
+            var bigMachine = container.Resolve<BigMachine<int>>();*/
+
+            var bigMachine = new BigMachine<int>(ThreadCore.Root);
 
             // Load
-            /*try
+            try
             {
                 using (var fs = new FileStream("app.data", FileMode.Open))
                 {
@@ -110,9 +116,9 @@ namespace ConsoleApp1
             }
             catch
             {
-            }*/
+            }
 
-            var testMachine = bigMachine.TryCreate<TestMachine.Interface>(3); // Machine is created using the interface class, not the machine class itself.
+            var testMachine = bigMachine.TryCreate<TestMachine.Interface>(3); // Machine is created via the interface class, not the machine class itself.
             if (testMachine != null)
             {
                 var currentState = testMachine.GetCurrentState(); // Get current state. You can operate machines using the interface class.
@@ -122,25 +128,18 @@ namespace ConsoleApp1
 
             testMachine = testGroup.TryGet<TestMachine.Interface>(3);
 
-            var testMachine2 = bigMachine.Create<TestMachine.Interface>(4);
-
             await ThreadCore.Root.WaitForTermination(-1); // Wait for the termination infinitely.
 
             // Save
-            /*var data = bigMachine.Serialize();
+            var data = bigMachine.Serialize();
             using (var fs = new FileStream("app.data", FileMode.Create))
             {
                 fs.Write(data);
             }
 
-            bigMachine.Deserialize(data);*/
+            bigMachine.Deserialize(data);
 
             ThreadCore.Root.TerminationEvent.Set(); // The termination process is complete (#1).
-        }
-
-        internal static void Other()
-        {
-
         }
     }
 }
