a512e0a488c2b6d5507445ae6764049fcf8311fc
archi-Doc <archidoc422@gmail.com>
2022-01-14 21:55:22 +0900

    0.16.2

--- BigMachinesGenerator/Arc.Visceral/VisceralIdentifier.cs
+++ BigMachinesGenerator/Arc.Visceral/VisceralIdentifier.cs
@@ -9,46 +9,45 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 #pragma warning disable SA1401 // Fields should be private
 #pragma warning disable SA1602 // Enumeration items should be documented
 
-namespace Arc.Visceral
+namespace Arc.Visceral;
+
+public class VisceralIdentifier
 {
-    public class VisceralIdentifier
+    public static VisceralIdentifier Default = new VisceralIdentifier();
+    // public static int IdentifierSerial = 0;
+
+    private HashSet<string> identifier = new();
+    private string identifierFormat = "__gen_visceral_identifier__";
+    private int identifierSerial = 0;
+
+    public VisceralIdentifier()
     {
-        public static VisceralIdentifier Default = new VisceralIdentifier();
-        // public static int IdentifierSerial = 0;
+    }
 
-        private HashSet<string> identifier = new();
-        private string identifierFormat = "__gen_visceral_identifier__";
-        private int identifierSerial = 0;
+    public VisceralIdentifier(string identifierFormat)
+    {
+        this.identifierFormat = identifierFormat;
+    }
 
-        public VisceralIdentifier()
+    public bool Add(string identifier) => this.identifier.Add(identifier);
+
+    public string GetIdentifier()
+    {
+        string identifier;
+        while (true)
         {
-        }
-
-        public VisceralIdentifier(string identifierFormat)
-        {
-            this.identifierFormat = identifierFormat;
-        }
-
-        public bool Add(string identifier) => this.identifier.Add(identifier);
-
-        public string GetIdentifier()
-        {
-            string identifier;
-            while (true)
+            if (this.identifierSerial++ < 1_000)
+            {// this.identifierSerial
+                identifier = this.identifierFormat + this.identifierSerial.ToString("D3");
+            }
+            else
             {
-                if (this.identifierSerial++ < 1_000)
-                {// this.identifierSerial
-                    identifier = this.identifierFormat + this.identifierSerial.ToString("D3");
-                }
-                else
-                {
-                    identifier = this.identifierFormat + this.identifierSerial.ToString();
-                }
+                identifier = this.identifierFormat + this.identifierSerial.ToString();
+            }
 
-                if (this.identifier.Add(identifier))
-                {
-                    return identifier;
-                }
+            if (this.identifier.Add(identifier))
+            {
+                return identifier;
             }
         }
     }
