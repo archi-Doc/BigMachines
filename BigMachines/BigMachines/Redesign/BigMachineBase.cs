d0ad9ebfc643d72a7990f4e9628d4016b8c9550a
archi-Doc <archidoc422@gmail.com>
2023-10-20 22:49:51 +0900

    .

---
+++ BigMachines/BigMachines/Redesign/BigMachineBase.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace BigMachines;
+
+public class BigMachineA : BigMachineBase
+{
+}
+
+public class BigMachineBase
+{
+}
