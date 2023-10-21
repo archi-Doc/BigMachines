d4f46dd771361473fbd8593e399a0a4ccbedda22
archi-Doc <archidoc422@gmail.com>
2023-10-21 23:39:43 +0900

    .....

--- BigMachines/BigMachines/Redesign/Control/MachineControl.cs
+++ BigMachines/BigMachines/Redesign/Control/MachineControl.cs
@@ -5,6 +5,40 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace BigMachines;
 
+public abstract class MachineControl
+{
+    public MachineControl(BigMachineBase bigMachine, MachineInfo<TIdentifier> info)
+    {
+        this.BigMachine = bigMachine;
+        this.Info = info;
+    }
+
+    #region FieldAndProperty
+
+    public BigMachineBase BigMachine { get; private set; }
+
+    public MachineInfo<TIdentifier> Info { get; private set; }
+
+    #endregion
+
+    #region Abstract
+
+    public abstract Machine<TIdentifier> GetOrAddMachine();
+
+    public abstract void AddMachine(IMachine machine);
+
+    public abstract bool TryGetMachine(TIdentifier identifier, [MaybeNullWhen(false)] out Machine<TIdentifier> machine);
+
+    public abstract bool RemoveFromGroup(Machine<TIdentifier> machine);
+
+    public abstract IEnumerable<TIdentifier> GetIdentifiers();
+
+    public abstract IEnumerable<Machine<TIdentifier>> GetMachines();
+
+    #endregion
+}
+
+
 public abstract class MachineControl<TIdentifier>
     where TIdentifier : notnull
 {
