557bf5be45949373b2398415e14eb4f3a2ae9907
archi-Doc <archidoc422@gmail.com>
2024-04-18 22:10:06 +0900

    .

---
+++ Playground/TinyMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Arc.Threading;
+using BigMachines;
+
+namespace Playground;
+
+[MachineObject]
+internal partial class TinyMachine : Machine
+{
+    public TinyMachine()
+        : base()
+    {
+        this.DefaultTimeout = TimeSpan.FromSeconds(1);
+    }
+
+    [StateMethod(0)]
+    public StateResult Initial(StateParameter parameter)
+    {
+        Console.WriteLine($"Tiny machine: {this.count++}");
+        if (this.count > 4)
+        {
+            return StateResult.Terminate;
+        }
+
+        return StateResult.Continue;
+    }
+
+    [CommandMethod]
+    protected CommandResult<int> Command1(int x)
+        => new(CommandResult.Success, x + 2);
+
+    protected override void OnTerminate()
+    {
+        Console.WriteLine($"Tiny machine: Terminated");
+        ThreadCore.Root.Terminate();
+    }
+
+    private int count;
+}
