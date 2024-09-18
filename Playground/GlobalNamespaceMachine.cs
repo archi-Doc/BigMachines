3e0308baccb6c7c88ff0e624cca6540d0640241e
archi-Doc <archidoc422@gmail.com>
2024-09-18 22:37:48 +0900

    .

---
+++ Playground/GlobalNamespaceMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BigMachines;
+
+[MachineObject]
+internal partial class GlobalNamespaceMachine : Machine
+{
+    public GlobalNamespaceMachine()
+        : base()
+    {
+        this.DefaultTimeout = TimeSpan.FromSeconds(1);
+    }
+
+    [StateMethod(0)]
+    public StateResult Initial(StateParameter parameter)
+    {
+        return StateResult.Terminate;
+    }
+}
