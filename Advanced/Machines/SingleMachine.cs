6cc3e036fc8c63701022b19fbf23d4623cd14152
archi-Doc <archidoc422@gmail.com>
2025-10-28 21:18:48 +0900

    .

--- Advanced/Machines/SingleMachine.cs
+++ Advanced/Machines/SingleMachine.cs
@@ -7,7 +7,8 @@ public partial class SingleMachine : Machine
 {// A machine without an identifier is derived from the Machine class.
     public static void Test(BigMachine bigMachine)
     {
-        bigMachine.SingleMachine.GetOrCreate(); // Only one machine is created.
+        bigMachine.SingleMachine.GetOrCreate("test"); // Only one machine is created.
+        bigMachine.SingleMachine.CreateAlways("test2"); // Terminate the previous machine and create a new one.
     }
 
     public SingleMachine()
@@ -16,6 +17,16 @@ public partial class SingleMachine : Machine
     }
 
     public int Count { get; set; }
+
+    protected override void OnCreate(object? createParam)
+    {
+        Console.WriteLine($"Single machine: Created('{createParam?.ToString()}')");
+    }
+
+    protected override void OnTerminate()
+    {
+        Console.WriteLine($"Single machine: Terminated");
+    }
 
     [StateMethod(0)]
     protected StateResult Initial(StateParameter parameter)
