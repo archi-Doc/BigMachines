101adce8ddb5e33f05d62fdb7c98d2333569be55
archi-Doc <archidoc422@gmail.com>
2026-04-24 00:07:37 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -45,7 +45,7 @@ class Program
         var tinyControl = bigMachine.TinyMachine;
         var machine = tinyControl.GetOrCreate();
 
-        await ThreadCore.Root.WaitForTerminationAsync(-1); // Wait for the termination infinitely.
+        await ThreadCore.Root.WaitForTermination(); // Wait for the termination infinitely.
 
         ThreadCore.Root.TerminationEvent.Set(); // The termination process is complete (#1).
         Console.WriteLine("Terminated.");
